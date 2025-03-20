cc407a2ba2bb39af169e312c80f6f07b9e21f8c3
Praneel Patel <prapatel26@gmail.com>
2025-03-20 15:11:13 -0400

    OCR code + prompting set
    no api yet

--- ocr/ocr_inference.py
+++ ocr/ocr_inference.py
@@ -14,54 +14,32 @@ text_tokenizer = model.get_text_tokenizer()
 visual_tokenizer = model.get_visual_tokenizer()
 
 # single-image input
+# image_path = r"C:\Users\prapa\Documents\GitHub\Eigen.Vector\ocr\example.png"
+# images = [Image.open(image_path)]
+# max_partition = 9
+# text = 'Describe the image.'
+# query = f'<image>\n{text}'
+
+math_background = "AI/ML Researcher"
+category_query = 1 
+category_text = "Does this have anything to do with PDEs?" #custom question
+if category_query == 1:
+    category_query = "You are an expert teacher with the purpose of clearly, concisely explaining though concepts thoroughly and step-by-step. Explain the concepts and ideas described in the attached image from the basics of my educational background, building up to the concepts employed, and a discussion of what is going on in the image itself. "
+elif category_query == 2: 
+    category_query = "Write a python script to visaulize what is described in the attached image."
+elif category_query == 3:
+    category_query = category_text
+
+query = "Write down the text in the attached image. " + str(category_query)
+cot_instructions = "Provide a step-by-step solution to the problem, and conclude with 'Now, my thinking is over. I'll tell my student the following:' followed by the final solution. My educational/skill level is " + str(math_background) + "; adjust your responses to frame from what I know to what is in the image."
+
+## cot-style input
+cot_suffix = cot_instructions
 image_path = r"C:\Users\prapa\Documents\GitHub\Eigen.Vector\ocr\example.png"
 images = [Image.open(image_path)]
 max_partition = 9
-text = 'Describe the image.'
-query = f'<image>\n{text}'
-
-## cot-style input
-# cot_suffix = "Provide a step-by-step solution to the problem, and conclude with 'the answer is' followed by the final solution."
-# image_path = '/data/images/example_1.jpg'
-# images = [Image.open(image_path)]
-# max_partition = 9
-# text = "What's the area of the shape?"
-# query = f'<image>\n{text}\n{cot_suffix}'
-
-## multiple-images input
-# image_paths = [
-#     '/data/images/example_1.jpg',
-#     '/data/images/example_2.jpg',
-#     '/data/images/example_3.jpg'
-# ]
-# images = [Image.open(image_path) for image_path in image_paths]
-# max_partition = 4
-# text = 'Describe each image.'
-# query = '\n'.join([f'Image {i+1}: <image>' for i in range(len(images))]) + '\n' + text
-
-## video input (require `pip install moviepy==1.0.3`)
-# from moviepy.editor import VideoFileClip
-# video_path = '/data/videos/example_1.mp4'
-# num_frames = 12
-# max_partition = 1
-# text = 'Describe the video.'
-# with VideoFileClip(video_path) as clip:
-#     total_frames = int(clip.fps * clip.duration)
-#     if total_frames <= num_frames:
-#         sampled_indices = range(total_frames)
-#     else:
-#         stride = total_frames / num_frames
-#         sampled_indices = [min(total_frames - 1, int((stride * i + stride * (i + 1)) / 2)) for i in range(num_frames)]
-#     frames = [clip.get_frame(index / clip.fps) for index in sampled_indices]
-#     frames = [Image.fromarray(frame, mode='RGB') for frame in frames]
-# images = frames
-# query = '\n'.join(['<image>'] * len(images)) + '\n' + text
-
-## text-only input
-# images = []
-# max_partition = None
-# text = 'Hello'
-# query = text
+text = query
+query = f'<image>\n{text}\n{cot_suffix}'
 
 # format conversation
 prompt, input_ids, pixel_values = model.preprocess_inputs(query, images, max_partition=max_partition)
@@ -76,10 +54,10 @@ pixel_values = [pixel_values]
 with torch.inference_mode():
     gen_kwargs = dict(
         max_new_tokens=1024,
-        do_sample=False,
-        top_p=None,
-        top_k=None,
-        temperature=None,
+        do_sample=True,
+        top_p=50,
+        top_k=0.9,
+        temperature=0.7,
         repetition_penalty=None,
         eos_token_id=model.generation_config.eos_token_id,
         pad_token_id=text_tokenizer.pad_token_id,
